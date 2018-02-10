f83e7017ce2cbab900909b42966cae9e9ce47a3a
Tasuku Takahahashi <lightfox.task@gmail.com>
2018-02-10 16:09:25 +0900

    Update for jenkins hook.

--- analyze/db_auth.py
+++ analyze/db_auth.py
@@ -21,7 +21,7 @@ import mysql.connector
 from mysql.connector.constants import ClientFlag
 
 auth_filename = "auth_info.txt"
-AUTH_SAVED_PATH = '/' 
+AUTH_SAVED_PATH = '/Users/tasuku/.aws/' 
 
 class SQLAuth:
     def __init__(self):
