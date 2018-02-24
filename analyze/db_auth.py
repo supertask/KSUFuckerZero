f49bc2e1150ce436d00f6981e0318feb99d1ae58
Tasuku Takahahashi <lightfox.task@gmail.com>
2018-02-25 04:52:26 +0900

    Update a little which is about creating index.

--- analyze/db_auth.py
+++ analyze/db_auth.py
@@ -21,8 +21,8 @@ import mysql.connector
 from mysql.connector.constants import ClientFlag
 
 auth_filename = "auth_info.txt"
-#AUTH_SAVED_PATH = '/Users/tasuku/.aws/' 
-AUTH_SAVED_PATH = '/'
+AUTH_SAVED_PATH = '/Users/tasuku/.aws/' 
+#AUTH_SAVED_PATH = '/'
 
 class SQLAuth:
     def __init__(self):
